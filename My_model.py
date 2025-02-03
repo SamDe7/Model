d49e8b00e4ce902a8364a2cb019186ab04c1e9d1
Denis <denisgoncarov06@gmail.com>
2025-02-03 16:04:17 +0700

    second commit

--- My_model.py
+++ My_model.py
@@ -1,1 +1,10 @@
-print('hi')
+import torch
+import torchvision
+
+from torchvision.datasets import DatasetFolder
+from torch.utils.data import DataLoader, Dataset, random_split
+
+import os
+import json
+import matplotlib.pyplot as plt
+import numpy as np
