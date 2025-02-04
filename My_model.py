38d6a20c19bd4856320f87f5a4413ede50acf187
Denis <denisgoncarov06@gmail.com>
2025-02-04 19:03:40 +0700

    After creation a hunder thousand images

--- My_model.py
+++ My_model.py
@@ -1,10 +1,12 @@
 import torch
 import torchvision
 
-from torchvision.datasets import DatasetFolder
-from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader, Dataset, random_split 
 
 import os
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+
+from PIL import Image
